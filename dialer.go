b54717dd1d5155059b0897c45b138e1c4ab82edc
Kooroshh <mr.oplus@yahoo.fr>
2021-10-24 20:44:21 +0330

    change module name

--- dialer.go
+++ dialer.go
@@ -4,9 +4,9 @@ import (
 	"errors"
 	"net"
 
-	"github.com/Dreamacro/go-shadowsocks2/core"
-	"github.com/Dreamacro/go-shadowsocks2/socks"
-	"github.com/Dreamacro/go-shadowsocks2/speeddial"
+	"github.com/kooroshh/go-shadowsocks2/core"
+	"github.com/kooroshh/go-shadowsocks2/socks"
+	"github.com/kooroshh/go-shadowsocks2/speeddial"
 )
 
 type Dialer interface {
--- tcp.go
+++ tcp.go
@@ -6,7 +6,7 @@ import (
 	"sync"
 	"time"
 
-	"github.com/Dreamacro/go-shadowsocks2/socks"
+	"github.com/kooroshh/go-shadowsocks2/socks"
 )
 
 func tcpKeepAlive(c net.Conn) {
